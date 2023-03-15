7b02514853d4d1d54828c0db42b3c06d4f765e9e
Mario Faundez <mariofaundez@hotmail.com>
2023-03-15 00:03:18 -0300

    chore: better description texts in app gradio

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import tensorflow_hub as hub
 import tensorflow_text as text
 import numpy as np
 np.set_printoptions(suppress=True)
-# https://www.kaggle.com/datasets/mrmorj/hate-speech-and-offensive-language-dataset
+
 labels = [
     "hate speech",
     "offensive language",
     "neither"
 ]
 
-# model
+
 with tf.keras.utils.custom_object_scope({'AdamWeightDecay': AdamWeightDecay(), 'WarmUp': WarmUp}):
     classifier_model = tf.keras.models.load_model('classifier_model.h5',
                                                   custom_objects={'KerasLayer': hub.KerasLayer})
@@ -28,13 +28,17 @@ examples = [
     ["This is wonderful!"],
 ]
 
+short_description = "This application classifies text into three categories: hate speech, offensive language, and neither, using a deep learning model trained on the Hate Speech and Offensive Language Dataset. Enter a sentence and the model will predict its category."
+
 hate_speech = gr.Interface(
     fn=run_model,
     inputs=gr.Textbox(lines=5,
-                      placeholder="Enter a positive or negative sentence here...",
+                      placeholder="Enter a sentence here...",
                       label="Input Text"),
     outputs=gr.outputs.Label(),
-    examples=examples
+    examples=examples,
+    title="Hate Speech and Offensive Language Classifier",
+    description=short_description
 )
 
 hate_speech.launch()
